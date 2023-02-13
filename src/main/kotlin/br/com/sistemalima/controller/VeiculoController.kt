2631b04a8f4a6c6ec81ded7acdcc7ff32b89b66b
wagnersistemalima <wagner.sistemalima@gmail.com>
2023-02-13 16:32:06 -0300

    ajustando controller

--- src/main/kotlin/br/com/sistemalima/controller/VeiculoController.kt
+++ src/main/kotlin/br/com/sistemalima/controller/VeiculoController.kt
@@ -4,11 +4,12 @@ import br.com.sistemalima.model.Observabilidade
 import br.com.sistemalima.model.Veiculo
 import br.com.sistemalima.service.VeiculoService
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.*
 import org.slf4j.LoggerFactory
 import java.util.*
 
-@Controller("/veiculos")
+@Controller(value = "/veiculos", produces = [MediaType.APPLICATION_JSON], consumes = [MediaType.APPLICATION_JSON])
 class VeiculoController(
     private val veiculoService: VeiculoService
 ) {
@@ -32,7 +33,7 @@ class VeiculoController(
         return HttpResponse.created(veiculo)
     }
 
-    @Get("/{id}")
+    @Get(value = "/{id}")
     fun findById(@PathVariable id: Long): HttpResponse<Veiculo> {
         val correlationId = UUID.randomUUID().toString()
         logger.info(String.format("$tag, $tagFindById, $tagStartMessage, id: $id, correlationId= $correlationId"))
